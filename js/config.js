57ebbb72d7b5a92f70e89cda544a8a8662bddf6c
Osvaldo Gago <opgago@yahoo.com>
2019-04-26 21:09:26 +0200

    Add files from reveal.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+
+    transition: 'convex',
+
+    hash: true,
+
+    history: true,
+
+    dependencies: [
+        { src: 'plugin/markdown/marked.js' },
+        { src: 'plugin/markdown/markdown.js' },
+        { src: 'plugin/notes/notes.js', async: true },
+        { src: 'plugin/zoom-js/zoom.js', async: true },
+        { src: 'plugin/highlight/highlight.js', async: true }
+    ]
+});
